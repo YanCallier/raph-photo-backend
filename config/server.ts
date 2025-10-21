6c8569a871e30fd2e76dcea3d5ace6939895d8b7
Yan Callier <yan.callier@gmail.com>
2025-10-21 12:37:25 +0200

    chore: initial Strapi ready for Render (Postgres/Cloud config)

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  url: env("PUBLIC_URL", ""), // on mettra l’URL Render après déploiement
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: { keys: env.array("APP_KEYS") },
 });
